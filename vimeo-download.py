7a3c094dd8ce42715d90d2dd41b901f96a174104
andy698087 <50565013+andy698087@users.noreply.github.com>
2019-06-03 02:14:09 +0800

    Update vimeo-download.py
    url safe for base64 decode.
    chagne (+,/) to  (-,_)

--- vimeo-download.py
+++ vimeo-download.py
@@ -67,7 +67,7 @@ def download_video(base_url, content):
 
     video_file = open(filename, 'wb')
 
-    init_segment = base64.b64decode(video['init_segment'])
+    init_segment = base64.urlsafe_b64decode(video['init_segment'])
     video_file.write(init_segment)
 
     for segment in tqdm(video['segments']):
